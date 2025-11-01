cb20a3b82d129d06c601cf58e4de47cd5ecb87eb
Andre <andre@familiesalwig.de>
2025-11-01 17:53:46 +0100

    add Extended info p to Content

--- src/app/lebenslauf/lebenslauf.component.ts
+++ src/app/lebenslauf/lebenslauf.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-lebenslauf',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lebenslauf.component.html',
   styleUrl: './lebenslauf.component.css'
 })
 export class LebenslaufComponent {
+
+  showSupervisorInfo = false;
+  showInformatikInfo = false;
+  showSignalIdunaInfo = false;
+  showMechatronikInfo = false;
+  showAbiInfo = false;
 
 }
